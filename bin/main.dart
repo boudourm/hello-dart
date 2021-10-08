f2479305349379fe8164a2adf40c609721440e27
boudourm <mehdiboudour.contact@gmail.com>
2021-10-09 00:08:38 +0200

    init [declarations, types, variables, const, final, booleans, concatenations]

---
+++ bin/main.dart
@@ -0,0 +1,35 @@
+
+
+main(List<String> arguments) { //entry point for execution
+  //Variable = spot in memory. Or just a bucket to hold a value
+
+  //
+  // var country; //string
+  // String name;
+  // name = "Paulo";
+  // country = "Japan";
+  // country = 23;
+  // print(country);
+
+  // int age = 13;
+  // double number = 23.1;
+  // print(number);
+
+  // bool isTrue = true;
+  // bool isFalse = false;
+  //
+  // print(isTrue);
+
+  // var country = "Jamaica";
+  // const pi = 3.14;
+
+  // String name = "James";
+  // String lastName = "Bond";
+  // int age = 45;
+  //
+  // print("$name $lastName is $age !");
+  // print("Hello there $name ${lastName.toUpperCase()}");
+
+  int number
+
+}
