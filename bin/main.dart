5989d0277ab82d78fabd8a609b1de55b3d44438e
boudourm <mehdiboudour.contact@gmail.com>
2021-10-09 00:24:31 +0200

    Equality and Relational Operators in Dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,43 +1,15 @@
 
 
 main(List<String> arguments) { //entry point for execution
-  //Variable = spot in memory. Or just a bucket to hold a value
 
-  //
-  // var country; //string
-  // String name;
-  // name = "Paulo";
-  // country = "Japan";
-  // country = 23;
-  // print(country);
-
-  // int age = 13;
-  // double number = 23.1;
-  // print(number);
-
-  // bool isTrue = true;
-  // bool isFalse = false;
-  //
-  // print(isTrue);
-
-  // var country = "Jamaica";
-  // const pi = 3.14;
-
-  // String name = "James";
-  // String lastName = "Bond";
-  // int age = 45;
-  //
-  // print("$name $lastName is $age !");
-  // print("Hello there $name ${lastName.toUpperCase()}");
-
-  int number = 34;
-  int numberTow = 2;
+  print(4 == 2);
 
   double pi = 3.14;
-  double gravity = 9.8;
+  print(pi == pi);
 
-  var result = number - numberTow;
-  print(gravity - pi);
+  double gravity = 9.8;
+  print(pi <= gravity);
+
 
 
 }
