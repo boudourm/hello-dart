077300e3f7961b242a13c4c6d061e173fef0afc1
boudourm <mehdiboudour.contact@gmail.com>
2021-10-09 00:42:26 +0200

    For Loops in Dart

--- bin/main.dart
+++ bin/main.dart
@@ -2,22 +2,10 @@
 
 main(List<String> arguments) { //entry point for execution
 
-int number = 34;
-int numberTow = 2;
 
-double pi = 3.14;
-double gravity = 9.8;
-
-if(!(number != 100) || number >= 100) {
-  print("Not a hundred !");
-}else{
-  print("Yess!");
-}
-
-if(!(number != 100) && number >= 100) {
-  print("Not a hundred !");
-}else{
-  print("Yess!");
-}
-
+  var fruit = "Mango";
+  for(var i = 0; i<10; i++){
+    if(i%2 == 0)
+      print("$fruit $i");
+  }
 }
