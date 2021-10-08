7a3eb2d909cbd4267e955e0dbb62ce4a3bc4bdf3
boudourm <mehdiboudour.contact@gmail.com>
2021-10-09 01:08:30 +0200

    Introduction to Functions in Dart

--- bin/main.dart
+++ bin/main.dart
@@ -2,12 +2,17 @@
 
 main(List<String> arguments) { //entry point for execution
 
-var age = 18;
-switch(age) {
-  case 15: print("too young"); break;
-  case 25: print("allowed to drink"); break;
-  case 55: print("too old"); break;
-  default : print("you're $age years old");
+  doSomething();
 }
 
+doSomething(){
+  print("Hello Functions!");
+  sayMyName();
+}
+sayMyName() {
+  print("Paulo");
+  var name = "Paulo";
+  if(name.contains("P"))
+    print("Horay!");
+  else print("Nay!");
 }
