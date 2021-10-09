66154fb1f8c1d13a24ef0c1188c421b079f84a3e
boudourm <mehdiboudour.contact@gmail.com>
2021-10-09 21:38:36 +0200

    Creating Lists and Iterating through Them

--- bin/main.dart
+++ bin/main.dart
@@ -1,52 +1,17 @@
 
-abstract class Animal{
-  void breathe(); //abstract method
-
-  void makeNoise(){
-    print("Making animal noises !");
-  }
-}
-
-abstract class IsFunny{
-  void makesPeopleLaught(); //abstract method
-}
-
-class TvShow implements IsFunny {
-  @override
-  void makesPeopleLaught() {
-    print("Broadcast a funny programm");
-  }
-
-}
-
-class Comedian extends Person implements IsFunny {
-  Comedian(String name, String nationality) : super(name, nationality);
-
-  @override
-  void makesPeopleLaught() {
-    print("Telling people jokes!");
-  }
-
-}
-
-class Person implements Animal {
-  late String name, nationality;
-
-
-  Person(this.name, this.nationality);
-
-  @override
-  void breathe() {
-    print("Breathing through norstrils!");
-  }
-
-  @override
-  void makeNoise() {
-    print("Shouting!");
-  }
-
-}
 
 main(List<String> arguments) { //entry point for execution
+  var list = [10, 2, 13, 24, 10, 56];
+  print(list[0]);
+  print(list[2]);
+  print(list[3]);
+  print(list.length);
+  print(list[list.length-1]);
+  for(int i=0; i<list.length;i++){
+    if(list[i]%2 == 0)
+      print("${list[i]} is multiple of 2.");
+    else
+      print("Index $i contains ${list[i]} and is not a multiple of 2.");
+  }
 
 }
