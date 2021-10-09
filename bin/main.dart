b10a6b56e3214650b6b181e83204dec03b040976
boudourm <mehdiboudour.contact@gmail.com>
2021-10-09 13:18:22 +0200

    Arguments and Functions in Dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,33 +1,10 @@
 
 
 main(List<String> arguments) { //entry point for execution
-
-  doSomething();
-
-  var name  = showName();
-  print(name);
+  var name = "James";
+  var lastName = "Bond";
+  var age = 65;
+  print(sayHello(name, lastName, age));
 }
 
-int showAge() => 34;
-
-String getName() => "James Bond";
-
-bool isKnow(){
-  return true;
-}
-
-String showName() {
-  return "Hello from ShowName";
-}
-
-void doSomething(){
-  print("Hello Functions!");
-  sayMyName();
-}
-sayMyName() {
-  print("Paulo");
-  var name = "Paulo";
-  if(name.contains("P"))
-    print("Horay!");
-  else print("Nay!");
-}
+String sayHello(String name, String lastname,   int age) => "Hello $name $lastname $age years o.";
