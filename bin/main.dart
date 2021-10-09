d8dd2b5d7e42ba5c53fa7ab810ec4387be872048
boudourm <mehdiboudour.contact@gmail.com>
2021-10-09 22:03:00 +0200

    Introduction to Maps

--- bin/main.dart
+++ bin/main.dart
@@ -1,36 +1,27 @@
 
-class Person{
-  late String name;
-
-  Person(this.name);
-
-  @override
-  String toString() {
-    return 'Person{name: $name}';
-  }
-}
-
-
 main(List<String> arguments) { //entry point for execution
 
-  var onlyStrings = new List.empty(growable: true);
-  onlyStrings.add("Paulo");
-  onlyStrings.add("James");
+  var winners = {
+    "first" : "Bill",
+    "second" : "George",
+    "third" : "Bond"
+  };
 
-  var personsList = new List.empty(growable: true);
-  personsList.add(new Person("Mehdi"));
-  personsList.add(new Person("Reda"));
-  personsList.add(new Person("Yacine"));
-  personsList.add(new Person("Fairouz"));
-  personsList.add(new Person("Hakim"));
+  var winnerSecond = {
+    1 : "Apollo",
+    2 : "Something",
+    3: "Another"
+  };
 
-  for(int i = 0; i<personsList.length; i++){
-    print(personsList[i]);
-  }
-  for(int i =0; i< onlyStrings.length; i++){
-    print(onlyStrings[i]);
-  }
+  var winnerThird = {
+    "Winter" : 3,
+    "Autumn" : 1,
+    "Spring" : 2
+  };
 
-
+  print(winners["second"]);
+  winners.forEach((key, value) => print("$key : $value"));
+  print(winners.keys);
+  print(winners.values);
 
 }
