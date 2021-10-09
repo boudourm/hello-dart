8ff0a6c2151d4077b066b04de81648985ae806e4
boudourm <mehdiboudour.contact@gmail.com>
2021-10-09 13:49:40 +0200

    Play Rewind 5s 2Playback rate Forward 5s 5:55 / 6:04 Adding Methods to Classes

--- bin/main.dart
+++ bin/main.dart
@@ -1,16 +1,49 @@
 
-var myOutsideVar = "Outside Var";
 
-main(List<String> arguments) { //entry point for execution
-  print(myOutsideVar);
-  myOutsideVar = "Hello world";
-  print(myOutsideVar);
+class Microphone{
+  //Instance variables
+  late String name;
+  late String color;
+  late int model;
+  late bool on = false;
 
-  insideFunction() {
-    var agentName = "Bond";
-    myOutsideVar = "James";
-    print("From inside is : $myOutsideVar");
+  //Methods
+  bool isOn() => on;
+
+  int modelNumber() => model;
+
+  void turnOn(){
+    on=true;
+    print("$name is on!");
   }
 
-  insideFunction();
+  void turnOff() {
+    on=false;
+    print("$name is turned off!");
+  }
+
+  void setVolume(){
+    print("$name with color : $color volume is up!");
+  }
+}
+
+main(List<String> arguments) { //entry point for execution
+
+  var mic = new Microphone();
+  mic.color = "blue";
+  mic.model = 2021;
+  mic.name = "Blue Yeti";
+
+  print(mic.model);
+  print(mic.name);
+  print(mic.color);
+
+  mic.turnOn();
+  print("${mic.name} is on : ${mic.isOn()}");
+  mic.setVolume();
+  mic.turnOff();
+  print("${mic.name} is on : ${mic.isOn()}");
+  print(mic.modelNumber());
+
+
 }
