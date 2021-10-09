cfb92a7f69e46b6421cf97c2a3c4440cb38c90f2
boudourm <mehdiboudour.contact@gmail.com>
2021-10-09 21:20:08 +0200

    Abstract and Interface Classes in Dart - Creation

--- bin/main.dart
+++ bin/main.dart
@@ -1,26 +1,52 @@
 
-class Location{
-  late num lat, lng;
+abstract class Animal{
+  void breathe(); //abstract method
 
-  Location(this.lat, this.lng);
-}
-
-class ElevatedLocation extends Location {
-  late num elevation;
-  ElevatedLocation(num lat, num lng) : super(lat, lng);
-
-  ElevatedLocation.withElevation(num lat, num lng, this.elevation) : super(lat, lng);
-
-  @override
-  String toString() {
-    return 'Location{lat: $lat, lng: $lng, elevation: $elevation}';
+  void makeNoise(){
+    print("Making animal noises !");
   }
 }
 
+abstract class IsFunny{
+  void makesPeopleLaught(); //abstract method
+}
+
+class TvShow implements IsFunny {
+  @override
+  void makesPeopleLaught() {
+    print("Broadcast a funny programm");
+  }
+
+}
+
+class Comedian extends Person implements IsFunny {
+  Comedian(String name, String nationality) : super(name, nationality);
+
+  @override
+  void makesPeopleLaught() {
+    print("Telling people jokes!");
+  }
+
+}
+
+class Person implements Animal {
+  late String name, nationality;
+
+
+  Person(this.name, this.nationality);
+
+  @override
+  void breathe() {
+    print("Breathing through norstrils!");
+  }
+
+  @override
+  void makeNoise() {
+    print("Shouting!");
+  }
+
+}
+
 main(List<String> arguments) { //entry point for execution
-  var elevated = new ElevatedLocation(23.89, -234.98);
-  print("location=(${elevated.lat},${elevated.lng})");
-  elevated = new ElevatedLocation.withElevation(65.45, 85.1, 456);
-  print("location=(${elevated.lat},${elevated.lng},${elevated.elevation})");
-  print(elevated);
+
 }
