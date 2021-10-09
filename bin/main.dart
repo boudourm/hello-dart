90b1af26d7f5be02bee57d41899f62e4e9db0b24
boudourm <mehdiboudour.contact@gmail.com>
2021-10-09 14:59:03 +0200

    Inheriting Classes with Constructors

--- bin/main.dart
+++ bin/main.dart
@@ -1,61 +1,20 @@
 
+class Location{
+  late num lat, lng;
 
-class Microphone{
-  //Instance variables
-  late String name;
-  late String color;
-  late int model;
-  late bool on = false;
+  Location(this.lat, this.lng);
+}
 
-  //Contructors
-  Microphone(this.name, this.color, this.model);
+class ElevatedLocation extends Location {
+  late num elevation;
+  ElevatedLocation(num lat, num lng) : super(lat, lng);
 
-  Microphone.intialize(){
-   this.name = "Blue Yeti 2nd Edition";
-   this.model = 2022;
-  }
-
-  //Getters & Setters
-  String get getName => name;
-  set setName(String name) => this.name=name;
-
-  String get getColor => color;
-  set setColor(String color) => this.color=color;
-
-  int get getModel => model;
-  set setModel(int model) => this.model=model;
-
-
-  //Methods
-  bool isOn() => on;
-
-  int modelNumber() => model;
-
-  void turnOn(){
-    on=true;
-    print("$name is on!");
-  }
-
-  void turnOff() {
-    on=false;
-    print("$name is turned off!");
-  }
-
-  void setVolume(){
-    print("$name with color : $color volume is up!");
-  }
+  ElevatedLocation.withElevation(num lat, num lng, this.elevation) : super(lat, lng);
 }
 
 main(List<String> arguments) { //entry point for execution
-
-  var micSecond = Microphone.intialize();
-  print(micSecond.name);
-  print(micSecond.model);
-
-  micSecond.setName = "Brown Yeti";
-
-  print(micSecond.getName);
-
-
-
+  var elevated = new ElevatedLocation(23.89, -234.98);
+  print("location=(${elevated.lat},${elevated.lng})");
+  elevated = new ElevatedLocation.withElevation(65.45, 85.1, 456);
+  print("location=(${elevated.lat},${elevated.lng},${elevated.elevation})");
 }
